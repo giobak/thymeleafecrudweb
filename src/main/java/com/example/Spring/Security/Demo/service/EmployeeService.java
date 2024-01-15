2bb44cda0ef997a2014e7b7566e693a61aafa578
giobak <giobaqradze@yahoo.com>
2024-01-15 21:13:16 +0100

    Initial commit

---
+++ src/main/java/com/example/Spring/Security/Demo/service/EmployeeService.java
@@ -0,0 +1,14 @@
+package com.example.Spring.Security.Demo.service;
+
+import com.example.Spring.Security.Demo.model.Employee;
+import org.springframework.data.domain.Page;
+
+import java.util.List;
+
+public interface EmployeeService {
+    List<Employee> getAllEmployees();
+    void saveEmployee(Employee employee);
+    Employee getEmployeeById(long id);
+    void deleteEmployeeById(long id);
+    Page<Employee> findPaginated(int pageNo, int pageSize, String sortField, String sortDirection);
+}
